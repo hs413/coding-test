4c9b7bbf9e54afe10c3ab655baed5bf71c89eb9b
rtg <rtg1539@naver.com>
2024-03-19 21:04:48 +0900

    level2 최댓값과 최솟값 - 최종 정답

---
+++ programmers/level-2/java/src/Test1.java
@@ -0,0 +1,36 @@
+import java.util.Arrays;
+import java.util.Map;
+import java.util.regex.Pattern;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
+/** 최댓값과 최솟값
+ * https://school.programmers.co.kr/learn/courses/30/lessons/12939
+ * 문자열 s에는 공백으로 구분된 숫자들이 저장되어 있습니다. str에 나타나는 숫자 중 최소값과 최대값을 찾아 이를 "(최소값) (최대값)"형태의 문자열을 반환하는 함수, solution을 완성하세요.
+ * 예를들어 s가 "1 2 3 4"라면 "1 4"를 리턴하고, "-1 -2 -3 -4"라면 "-4 -1"을 리턴하면 됩니다.
+* */
+public class Test1 {
+    public static void main(String[] args) {
+//        "1 2 3 4"	"1 4"
+//        "-1 -2 -3 -4"	"-4 -1"
+//        "-1 -1"	"-1 -1"
+
+//        String s = "1 2 3 4";
+        String s = "-1 -2 -3 -4";
+//        String s = "-1 -1";
+
+        int min = Integer.MAX_VALUE, max = Integer.MIN_VALUE;
+        String[] arr = s.split(" ");
+
+        for (String str : arr) {
+            int cur = Integer.parseInt(str);
+            min = Math.min(min, cur);
+            max = Math.max(max, cur);
+        }
+
+        String answer = min + " " + max;
+//        return min + " " + max;
+
+        System.out.println();
+    }
+}
