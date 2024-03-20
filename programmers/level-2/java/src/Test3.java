bad934113ca015bc21fd5431e55f4ca605b3967b
rtg <rtg1539@naver.com>
2024-03-20 11:11:30 +0900

    level2 최댓값과 최솟값 - 다른 사람 풀이

--- programmers/level-2/java/src/Test3.java
+++ programmers/level-2/java/src/Test3.java
@@ -33,4 +33,30 @@ public class Test3 {
 //      return answer ;
         System.out.println();
     }
+
+    // 다른 사람 풀이 퀵정렬
+    public static void quickSort(int[] arr, int left, int right)
+    {
+        int i, j, pivot, tmp;
+
+        if (left < right) {
+            i = left;
+            j = right;
+            pivot = arr[left];
+            //분할 과정
+            while (i < j) {
+                while (arr[j] > pivot) j--;
+                while (i < j && arr[i] <= pivot) i++;
+
+                tmp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = tmp;
+            }
+            arr[left] = arr[i];
+            arr[i] = pivot;
+            //정렬 과정
+            quickSort(arr, left, i - 1);
+            quickSort(arr, i + 1, right);
+        }
+    }
 }
