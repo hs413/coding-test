ab752de7c6c555c153bddc7d5987d193528eebdf
rtg <rtg1539@naver.com>
2024-03-20 12:05:59 +0900

    level2 올바른 괄호 - 최종 정답

---
+++ programmers/level-2/java/src/Test4.java
@@ -0,0 +1,34 @@
+/** 올바른 괄호
+ * https://school.programmers.co.kr/learn/courses/30/lessons/12909
+ * 괄호가 바르게 짝지어졌다는 것은 '(' 문자로 열렸으면 반드시 짝지어서 ')'문자로 닫혀야 한다는 뜻입니다.
+ * "()()" 또는 "(())()" 는 올바른 괄호입니다.
+ * ")()(" 또는 "(()(" 는 올바르지 않은 괄호입니다.
+ * '(' 또는 ')' 로만 이루어진 문자열 s가 주어졌을 때,
+ * 문자열 s가 올바른 괄호이면 true를 return 하고,
+ * 올바르지 않은 괄호이면 false를 return 하는 solution 함수를 완성해 주세요.
+ * */
+public class Test4 {
+    public static void main(String[] args) {
+//        String s = "()()";	//true
+//         String s = "(())()";	//true
+         String s = ")()(";	//false
+        // String s = "(()(";	//false
+
+        String[] arr = s.split("");
+
+        int n = 0;
+        for (String str : arr) {
+            if (str.equals("(")) n += 1;
+
+            if (str.equals(")")) n -= 1;
+
+            if (n < 0) {
+                break;
+            }
+        }
+
+        boolean answer = n == 0;
+//        return n == 0;
+        System.out.println();
+    }
+}
