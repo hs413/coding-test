06be17b40f8d05f4a0dfff444a27b111be9c454f
rtg <rtg1539@naver.com>
2024-03-20 21:16:05 +0900

    level2 카펫 - 최종 정답

---
+++ programmers/level-2/java/src/Test8.java
@@ -0,0 +1,42 @@
+import java.util.ArrayList;
+import java.util.List;
+
+/** 카펫
+ * https://school.programmers.co.kr/learn/courses/30/lessons/42842
+ * */
+public class Test8 {
+    public static void main(String[] args) {
+//        int brown = 10; //[4, 3]
+//        int brown = 8; //[3, 3]
+//        int brown = 16; //[8, 6]
+        int brown = 18; //[8, 6]
+//        int yellow = 2;
+//        int yellow = 1;
+//        int yellow = 5;
+        int yellow = 6;
+
+        List<Integer> list = new ArrayList<>();
+        int all = brown + yellow;
+        int limit = brown / 2;
+
+        for (int i = 3 ; i < limit; i++) {
+            if (all % i == 0) list.add(i);
+        }
+
+        int[] answer = new int[2];
+
+        for (Integer row : list) {
+            int col = all / row;
+
+            if (yellow == (row - 2) * (col - 2)) {
+                answer[0] = col;
+                answer[1] = row;
+                break;
+            }
+        }
+
+        System.out.println("");
+
+//        return answer;
+    }
+}
