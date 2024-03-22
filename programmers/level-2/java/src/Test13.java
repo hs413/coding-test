ceb4a33cd95bb41d3c9c6ea14dbc3e07cae6758d
rtg <rtg1539@naver.com>
2024-03-22 14:25:48 +0900

    level2 H-Index - 다른 사람 풀이

--- programmers/level-2/java/src/Test13.java
+++ programmers/level-2/java/src/Test13.java
@@ -34,4 +34,17 @@ public class Test13 {
 
         System.out.println();
     }
+
+    // 다른 사람 풀이
+    public int solution(int[] citations) {
+        Arrays.sort(citations);
+
+        int max = 0;
+        for(int i = citations.length-1; i > -1; i--){
+            int min = (int)Math.min(citations[i], citations.length - i);
+            if(max < min) max = min;
+        }
+
+        return max;
+    }
 }
