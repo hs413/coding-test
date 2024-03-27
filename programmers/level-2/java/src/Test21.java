a6603b7ab8831f94fb40b5d4f1d2d8bbd1791d24
rtg <rtg1539@naver.com>
2024-03-27 21:59:50 +0900

    level2 큰 수 만들기 - 다른 사람 풀이

--- programmers/level-2/java/src/Test21.java
+++ programmers/level-2/java/src/Test21.java
@@ -43,5 +43,23 @@ public class Test21 {
 
         return sb.substring(0, number.length() - kClone);
     }
+
+    // 다른 사람 풀이 (스택 사용)
+    /*public String solution(String number, int k) {
+        char[] result = new char[number.length() - k];
+        Stack<Character> stack = new Stack<>();
+
+        for (int i=0; i<number.length(); i++) {
+            char c = number.charAt(i);
+            while (!stack.isEmpty() && stack.peek() < c && k-- > 0) {
+                stack.pop();
+            }
+            stack.push(c);
+        }
+        for (int i=0; i<result.length; i++) {
+            result[i] = stack.get(i);
+        }
+        return new String(result);
+    }*/
 }
 
